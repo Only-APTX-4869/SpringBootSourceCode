83990c1aaeb095813824b2194fe4747d48265898
zhenhua <1641605908@qq.com>
2022-06-29 14:25:47 +0800

    '20220629' add readFileOrFolder

--- Jpa/src/main/java/com/czh/controller/DealFile.java
+++ Jpa/src/main/java/com/czh/controller/DealFile.java
@@ -1,18 +1,48 @@
 package com.czh.controller;
 
-import java.io.File;
+import java.io.*;
 
 public class DealFile {
+    private static File file;
 
-    public static void main(String[] args) {
-        File file = new File("D:\\workspace_Idea\\SpringBootSourceCode\\Jpa\\src\\main\\resources");
-        readFile(file);
+    public static void main(String[] args) throws IOException {
+        file = new File("D:\\workspace_Idea\\SpringBootSourceCode\\Jpa\\src\\main\\resources");
+        readFileController(file);
     }
 
-    private static void readFile(File file) {
-        File[] files = file.listFiles();
-        for (File fileName : files) {
-            fileName.getName();
+    private static void readFileController(File file) throws IOException {
+        if (file.isDirectory()) {
+            String[] files = file.list();
+            for (String fileName : files) {
+                File f = new File(file + "\\" + fileName);
+                if (f.isDirectory()) {
+                    readFileController(f);
+                } else {
+                    readFileService(f);
+                }
+            }
+        } else {
+            readFileService(file);
         }
+
     }
+
+    private static void readFileService(File f) throws IOException {
+        FileInputStream fis = new FileInputStream(f);
+        FileOutputStream fos = new FileOutputStream(file.getCanonicalFile() + "\\sunText.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis, "utf-8"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fos, "UTF-8"));
+        String readLine = "";
+        while ((readLine = bufferedReader.readLine()) != null) {
+            bufferedWriter.write(readLine);
+        }
+        if (readLine == null) {
+            bufferedWriter.write("/n ------------该文件读取完毕----------/n" + f);
+        }
+        bufferedReader.close();
+        bufferedWriter.close();
+        fis.close();
+        fos.close();
+    }
+
 }
--- Jpa/src/main/java/com/czh/util/parameterException.java
+++
@@ -1,7 +0,0 @@
-package com.czh.util;
-
-public class parameterException extends Exception {
-    parameterException(String error) {
-        super(error);
-    }
-}
